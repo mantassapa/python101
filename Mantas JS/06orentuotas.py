e7c38808403e8faef6342f7ea3bd9630b67e87dd
mantassapa <71067404+mantassapa@users.noreply.github.com>
2023-09-25 08:07:23 +0300

    Add files via upload

---
+++ Mantas JS/06orentuotas.py
@@ -0,0 +1,15 @@
+x =[5, 6, 8]
+print(x)
+print(x[1])
+a = x
+print(a)
+x[1] = 100
+print('x=',x)
+print('a=',a)
+
+# c = 5
+# d = c
+# c = 100
+# print(c)
+# print(d) 
+
---
+++ Mantas JS/11dezes.py
@@ -0,0 +1,8 @@
+p = int(input('kiekis'))
+d = int(input('kiekis i didele deze'))
+e = int(input('kiekis i maza deze'))
+dkiek = p // d
+likutis = p % d
+ekiek = likutis // e
+liko = likutis % e
+print(f'reikes {dkiek} dideliu deziu, {ekiek} mazu deziu {liko} prekiu ')
