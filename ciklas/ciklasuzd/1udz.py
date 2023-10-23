2ebb342b1165439aec2b7acc4fe6b4e2f91de2d0
mantassapa <71067404+mantassapa@users.noreply.github.com>
2023-10-23 11:29:27 +0300

    Update 1udz.py

--- ciklas/ciklasuzd/1udz.py
+++ ciklas/ciklasuzd/1udz.py
@@ -1,9 +1,10 @@
 prad = int(input('iveskite pradine kaina...'))
 moneta = int(input('imeskite moneta...'))
 suma = 0
-if suma<=prad: 
-    suma += moneta
-    moneta = int(input('imeskite dar moneta'))
-    print('uz kava atsikaityta')
-else:
-    return moneta
+while suma < prad
+    if
+        suma += moneta
+        moneta = int(input('imeskite dar moneta'))
+        print('uz kava atsikaityta')
+    else:
+    p
