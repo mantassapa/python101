14ad32cf8be8d102d7bdf51440cf1a74132444ad
mantassapa <mantassapa@gmail.com>
2023-12-10 16:58:50 +0200

    no

--- sarasas/uzd/5.2uzd.py
+++ sarasas/uzd/5.2uzd.py
@@ -1,16 +1,10 @@
 a = [2, 4, 6, 8, 10, 12, 14]
 b = [1, 3, 5, 7, 9, 11, 13]
 c = []
-a.reverse()
-b.reverse()
-padetis = 0
+
 for i in range(len(a)):
-    A = a.pop(i)
-    c.append(A)
-    B = b.pop(i)
-    c.append(B)
-print(c)
-
-
-
+    d = a.pop(0)
+    c.append(d)
+    e = b.pop(0)
+    c.append(e)
 print(c)
